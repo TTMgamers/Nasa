80288183c662fce3afefafa3c59028bdc021e1d7
TTMgamers <92926856+TTMgamers@users.noreply.github.com>
2021-10-28 07:49:16 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna pre
 global.player = ['62822648654777']
 global.APIs = { // API Prefix
   // nama: 'https://website'
-  Nasabot: 'https://rest-api-nasabot.herokuapp.com'
   bx: 'https://bx-hunter.herokuapp.com',
   hardianto: 'https://hardianto-chan.herokuapp.com',
   jonaz: 'https://jonaz-api-v2.herokuapp.com',
@@ -19,7 +18,6 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey nya disini
   // 'https://website': 'apikey'
-  ' https://rest-api-nasabot.herokuapp.com': 'Nasabot'
   'https://bx-hunter.herokuapp.com': 'Ikyy69',
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
